6cec7f7c79770518e75745b79c884383a2863399
Spyros Bazaios <sbazaios@gmail.com>
2012-06-01 03:09:20 +0300

    indent using 2 spaces

--- footer.php
+++ footer.php
@@ -1,10 +1,9 @@
-	<footer>
-		<p>Cosmos Theme by nautilus7 - Powered from Wordpress</p>
-	</footer>
+  <footer>
+    <p>Cosmos Theme by nautilus7 - Powered by Wordpress</p>
+  </footer>
 
 </div><!-- /#wrapper -->
 
 <?php wp_footer(); ?>
-
 </body>
 </html>
