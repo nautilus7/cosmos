a5e5b79bc3b354d3639c55ed26267d040ff88640
Spyros Bazaios <sbazaios@gmail.com>
2012-06-01 03:26:22 +0300

    Small fix to nav menu elemnts.
    Parent elemnts in nav bar menu use # as ancherstor now.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 /* Additional Theme Java Scripts
  * Author: nautilus7, 2012
  */
+
+// Replace parent href attribute with # in navbar menu
+$(document).ready(function () { $('nav > ul > li:has(ul) > a').attr('href', function (i, val) { return val + '#'; }); });
