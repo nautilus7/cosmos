047835e3ccc278441db7b88f7a0c83c077e53ed8
Spyros Bazaios <sbazaios@gmail.com>
2012-06-07 20:09:51 +0300

    update NHP options framework to 1.0.6

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-locate_template('/theme-options.php' , true, true); // theme options
+locate_template('nhp-options.php'    , true, true); // theme options
 locate_template('/inc/cleanup.php'   , true, true); // head cleanup
 locate_template('/inc/initialize.php', true, true); // initialize
 locate_template('/inc/content.php'   , true, true); // post meta
--- nhp-options.php
+++ nhp-options.php
@@ -2,8 +2,10 @@
 /*
  * 
  * Require the framework class before doing anything else, so we can use the defined urls and dirs
+ * Also if running on windows you may have url problems, which can be fixed by defining the framework url first
  *
  */
+//define('NHP_OPTIONS_URL', site_url('path the options folder'));
 if(!class_exists('NHP_Options')){
 	require_once( dirname( __FILE__ ) . '/options/options.php' );
 }
@@ -73,6 +75,9 @@ $args = array();
 //Set it to dev mode to view the class settings/info in the form - default is false
 $args['dev_mode'] = true;
 
+//google api key MUST BE DEFINED IF YOU WANT TO USE GOOGLE WEBFONTS
+//$args['google_api_key'] = '***';
+
 //Remove the default stylesheet? make sure you enqueue another one all the page will look whack!
 //$args['stylesheet_override'] = true;
 
@@ -107,16 +112,26 @@ $args['menu_title'] = __('Theme Options', 'nhp-opts');
 $args['page_title'] = __('Twenty Eleven Theme Options', 'nhp-opts');
 
 //Custom page slug for options page (wp-admin/themes.php?page=***) - default is "nhp_theme_options"
-$args['page_slug'] = 'theme_options';
+$args['page_slug'] = 'nhp_theme_options';
 
 //Custom page capability - default is set to "manage_options"
 //$args['page_cap'] = 'manage_options';
 
+//page type - "menu" (adds a top menu section) or "submenu" (adds a submenu) - default is set to "menu"
+//$args['page_type'] = 'submenu';
+
+//parent menu - default is set to "themes.php" (Appearance)
+//the list of available parent menus is available here: http://codex.wordpress.org/Function_Reference/add_submenu_page#Parameters
+//$args['page_parent'] = 'themes.php';
+
 //custom page location - default 100 - must be unique or will override other items
-//$args['page_position'] = 10;
+$args['page_position'] = 27;
 
 //Custom page icon class (used to override the page icon next to heading)
 //$args['page_icon'] = 'icon-themes';
+
+//Want to disable the sections showing as a submenu in the admin? uncomment this line
+//$args['allow_sub_menu'] = false;
 		
 //Set ANY custom page help tabs - displayed using the new help tab API, show in order of definition		
 $args['help_tabs'][] = array(
@@ -292,6 +307,15 @@ $sections[] = array(
 						'sub_desc' => __('Can also use the validation methods if required', 'nhp-opts'),
 						'desc' => __('This is the description field, again good for additional info.', 'nhp-opts'),
 						'std' => 'OOOOOOhhhh, rich editing.'
+						)
+					,
+					array(
+						'id' => 'editor2',
+						'type' => 'editor',
+						'title' => __('Editor Option 2', 'nhp-opts'), 
+						'sub_desc' => __('Can also use the validation methods if required', 'nhp-opts'),
+						'desc' => __('This is the description field, again good for additional info.', 'nhp-opts'),
+						'std' => 'OOOOOOhhhh, rich editing2.'
 						)
 					)
 				);
@@ -538,7 +562,14 @@ $sections[] = array(
 						'sub_desc' => __('This is a completely unique field type', 'nhp-opts'),
 						'desc' => __('This is created with a callback function, so anything goes in this field. Make sure to define the function though.', 'nhp-opts'),
 						'callback' => 'my_custom_field'
-						)								
+						),
+					array(
+						'id' => 'google_webfonts',
+						'type' => 'google_webfonts',//doesnt need to be called for callback fields
+						'title' => __('Google Webfonts', 'nhp-opts'), 
+						'sub_desc' => __('This is a completely unique field type', 'nhp-opts'),
+						'desc' => __('This is a simple implementation of the developer API for Google webfonts. Preview selection will be coming in future releases.', 'nhp-opts')
+						)							
 					)
 				);
 
@@ -582,14 +613,14 @@ $sections[] = array(
 				
 				
 	$tabs = array();
-	
+			
 	if (function_exists('wp_get_theme')){
 		$theme_data = wp_get_theme();
-		$theme_uri = $theme_data->ThemeURI;
-		$description = $theme_data->Description;
-		$author = $theme_data->Author;
-		$version = $theme_data->Version;
-		$tags = $theme_data->Tags;
+		$theme_uri = $theme_data->get('ThemeURI');
+		$description = $theme_data->get('Description');
+		$author = $theme_data->get('Author');
+		$version = $theme_data->get('Version');
+		$tags = $theme_data->get('Tags');
 	}else{
 		$theme_data = get_theme_data(trailingslashit(get_stylesheet_directory()).'style.css');
 		$theme_uri = $theme_data['URI'];
@@ -606,20 +637,22 @@ $sections[] = array(
 	$theme_info .= '<p class="nhp-opts-theme-data description theme-description">'.$description.'</p>';
 	$theme_info .= '<p class="nhp-opts-theme-data description theme-tags">'.__('<strong>Tags:</strong> ', 'nhp-opts').implode(', ', $tags).'</p>';
 	$theme_info .= '</div>';
-	
-	
-	
+
+
+
 	$tabs['theme_info'] = array(
 					'icon' => NHP_OPTIONS_URL.'img/glyphicons/glyphicons_195_circle_info.png',
 					'title' => __('Theme Information', 'nhp-opts'),
 					'content' => $theme_info
 					);
-					
-	$tabs['theme_docs'] = array(
-					'icon' => NHP_OPTIONS_URL.'img/glyphicons/glyphicons_071_book.png',
-					'title' => __('Documentation', 'nhp-opts'),
-					'content' => nl2br(file_get_contents(trailingslashit(get_stylesheet_directory()).'README.txt'))
-					);
+	
+	if(file_exists(trailingslashit(get_stylesheet_directory()).'README.html')){
+		$tabs['theme_docs'] = array(
+						'icon' => NHP_OPTIONS_URL.'img/glyphicons/glyphicons_071_book.png',
+						'title' => __('Documentation', 'nhp-opts'),
+						'content' => nl2br(file_get_contents(trailingslashit(get_stylesheet_directory()).'README.html'))
+						);
+	}//if
 
 	global $NHP_Options;
 	$NHP_Options = new NHP_Options($sections, $args, $tabs);
@@ -646,7 +679,7 @@ function my_custom_field($field, $value){
 function validate_callback_function($field, $value, $existing_value){
 	
 	$error = false;
-	$value = 'just testing';
+	$value =  'just testing';
 	/*
 	do your validation
 	
