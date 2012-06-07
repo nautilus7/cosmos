12cd8b23d3d8f969980234ddb229f2ebccdf5b95
Spyros Bazaios <sbazaios@gmail.com>
2012-06-07 20:12:40 +0300

    remove image and video dimensions from output
    Remove width and height attributes from <img>, <video> and other html
    elements.

--- inc/cleanup.php
+++ inc/cleanup.php
@@ -27,6 +27,20 @@ function cosmos_remove_recent_comments_style() {
 }
 add_action('widgets_init', 'cosmos_remove_recent_comments_style', 10, 0);
 
+
+function cosmos_remove_img_dimensions($html) {
+
+	$html = preg_replace('/(width|height)="\d*"\s/', '', $html);
+	return $html;
+}
+add_filter('the_content'         , 'cosmos_remove_img_dimensions');
+add_filter('post_thumbnail_html' , 'cosmos_remove_img_dimensions');
+add_filter('image_send_to_editor', 'cosmos_remove_img_dimensions');
+
+
+
+
+
 // URL rewrite of search results page
 function search_url_rewrite_rule() {
 
