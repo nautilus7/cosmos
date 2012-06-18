b6dc3e27a47c9b37673f63eb0b1ca5dd393fe041
Spyros Bazaios <sbazaios@gmail.com>
2012-06-18 13:21:42 +0300

    cleanup includes
    1. Remove some deprecated action hooks from wp_head
    2. Simplify remove_img_dimensions() functions
    3. Rework search_redirect() function according to roots theme

--- inc/cleanup.php
+++ inc/cleanup.php
@@ -6,14 +6,10 @@ show_admin_bar(false);
 // clean Wordpress Head Section
 remove_action('wp_head', 'rsd_link');
 remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
 remove_action('wp_head', 'wp_generator');
 remove_action('wp_head', 'feed_links', 2);
 remove_action('wp_head', 'feed_links_extra', 3);
-remove_action('wp_head', 'index_rel_link');
-remove_action('wp_head', 'parent_post_rel_link', 10, 0);
-remove_action('wp_head', 'start_post_rel_link', 10, 0);
-remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
-remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
 
 // Remove Recent Comments Widget CSS Style
 function cosmos_remove_recent_comments_style() {
@@ -30,34 +26,21 @@ add_action('widgets_init', 'cosmos_remove_recent_comments_style', 10, 0);
 
 function cosmos_remove_img_dimensions($html) {
 
-	$html = preg_replace('/(width|height)="\d*"\s/', '', $html);
-	return $html;
+	return preg_replace('/(width|height)="\d*"\s/', '', $html); 
 }
 add_filter('the_content'         , 'cosmos_remove_img_dimensions');
 add_filter('post_thumbnail_html' , 'cosmos_remove_img_dimensions');
 add_filter('image_send_to_editor', 'cosmos_remove_img_dimensions');
 
+// Nice redirect of the search page
+function cosmos_search_redirect() {
 
+	global $wp_rewrite;
 
-
-
-// URL rewrite of search results page
-function search_url_rewrite_rule() {
-
-	if ( is_search() && !empty($_GET['s']) ) {
-
-		wp_redirect(home_url("/search/") . urlencode(get_query_var('s')));
-		exit();
-	}
-}
-add_action('template_redirect', 'search_url_rewrite_rule');
-
-function roots_nice_search_redirect() {
-
-	if (is_search() && strpos($_SERVER['REQUEST_URI'], '/wp-admin/') === false && strpos($_SERVER['REQUEST_URI'], '/search/') === false) {
+	if ( $wp_rewrite->using_permalinks() && is_search() && !empty($_GET['s']) ) {
 
 		wp_redirect(home_url('/search/' . str_replace(array(' ', '%20'), array('+', '+'), urlencode(get_query_var('s')))), 301);
 		exit();
 	}
 }
-add_action('template_redirect', 'roots_nice_search_redirect');
+add_action('template_redirect', 'cosmos_search_redirect');
