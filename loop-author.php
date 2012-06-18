dec94ed0aa4fd871e720e3cdffdef168a0fb0c56
Spyros Bazaios <sbazaios@gmail.com>
2012-06-18 13:30:21 +0300

    Add new template for the author's archive

---
+++ loop-author.php
@@ -0,0 +1,32 @@
+<?php if ( have_posts() ) : ?>
+
+	<article class="author-bio well clearfix">
+		<h1><?php _e('Author: ', 'cosmos'); the_author_link(); ?></h1>
+		<?php if ( get_avatar(get_the_author_meta('ID')) ) : ?>
+			<div class="gravatar"><?php echo get_avatar(get_the_author_meta('ID')); ?></div>
+		<?php endif; ?>
+		<?php if ( get_the_author_meta('user_description') ) : ?>
+			<p><?php the_author_meta('user_description'); ?></p>
+		<?php endif; ?>
+	</article>
+
+<?php while ( have_posts() ) : the_post(); ?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+	<div class="post-meta">
+		<?php cosmos_post_date(); cosmos_post_category(); cosmos_post_tag(); cosmos_post_comments(); cosmos_post_thumbnail(); ?>
+	</div>
+		<?php the_excerpt(); ?>
+
+</article>
+
+<?php endwhile; ?>
+
+<?php cosmos_page_nav(); ?>
+
+<?php else: ?>
+
+<p><?php _e('The author archive is empty', 'cosmos'); ?></p>
+
+<?php endif; ?>
