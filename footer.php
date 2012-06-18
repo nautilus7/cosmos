f6b7d35fec5e827f37684a92932cc37dc83edcf6
Spyros Bazaios <sbazaios@gmail.com>
2012-06-18 13:17:20 +0300

    Split footer section in two parts

--- footer.php
+++ footer.php
@@ -1,5 +1,10 @@
-	<footer>
-		<p>Cosmos Theme by nautilus7 - Powered by Wordpress</p>
+	<footer class="row-fluid">
+		<div class="span6">
+			<p>Cosmos Theme by nautilus7</p>
+		</div>
+		<div class="span6">
+			<p>Powered by Wordpress</p>
+		</div>
 	</footer>
 </div><!-- /#wrapper -->
 
