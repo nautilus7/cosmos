9305571525df79b65110f28bf9ff5b06a7e1577e
Spyros Bazaios <sbazaios@gmail.com>
2012-06-18 13:28:55 +0300

    Re-form template
    Nothing important changed here. Just the design.

--- loop.php
+++ loop.php
@@ -3,18 +3,23 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header>
 		<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+		<div class="post-meta clearfix">
+			<?php cosmos_post_author(array('text' => 'by ', 'icon' => false));
+			cosmos_post_date();
+			cosmos_post_category();
+			cosmos_post_tag();
+			cosmos_post_comments(); ?>
+		</div>
 	</header>
 	<div class="row-fluid">
 		<div class="span3">
-			<?php cosmos_post_thumbnail(); ?>
+			<?php cosmos_post_thumbnail(array('size' => 'archive')); ?>
 		</div>
 		<div class="span9">
 			<?php the_excerpt(); ?>
+			<a href="<?php esc_url(the_permalink()); ?>" title="" class="btn"><?php esc_html_e('Continue Reading', 'cosmos'); ?></a>
 		</div>
 	</div>
-	<footer>
-		<?php cosmos_post_author(); cosmos_post_date(); cosmos_post_category(); cosmos_post_tag(); cosmos_post_comments(); ?>
-	</footer>
 </article>
 
 <?php endwhile; ?>
