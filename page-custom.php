e35e87192fc5cebe73aa6bc1ce118188017259f2
Spyros Bazaios <sbazaios@gmail.com>
2012-06-19 13:33:37 +0300

    Add sample page template
    A sample template for new page layouts. Nothing important here. Modify
    it to your needs.

---
+++ page-custom.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Template Name: Custom Page
+ * Description: A Page Template to customize to your needs
+ *
+ * @package Cosmos
+ * @since 0.3
+ */
+
+get_header(); ?>
+
+<?php get_template_part('loop', 'page'); ?>
+
+<?php get_footer(); ?>
