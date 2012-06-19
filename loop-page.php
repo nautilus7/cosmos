939666a92666808a96a0679de5a1e48d9e3fb861
Spyros Bazaios <sbazaios@gmail.com>
2012-06-19 13:31:58 +0300

    Add new template for pages and corresponding loop

---
+++ loop-page.php
@@ -0,0 +1,15 @@
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<header class="page-header">
+		<h1><?php the_title(); cosmos_post_author(array('before' => '<small>', 'after' => '</small>', 'text' => ' by ', 'icon' => false)); ?></h1>
+	</header>
+
+	<?php the_content(); ?>
+	<?php comments_template(); ?>
+
+</article>
+
+<?php endwhile; ?>
+<?php endif; ?>
---
+++ page.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+	<div id="content" class="row-fluid">
+		<div id="main" class="span8">
+			<?php get_template_part('loop', 'page'); ?>
+		</div>
+		<aside id="sidebar-right" class="span4">
+			<?php get_sidebar('right'); ?>
+		</aside>
+	</div>
+
+	<?php get_sidebar('bottom'); ?>
+
+<?php get_footer(); ?>
