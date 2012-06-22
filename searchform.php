4bdd0dba188c4479e075deea883526160b37fc80
Spyros Bazaios <sbazaios@gmail.com>
2012-06-22 15:19:35 +0300

    Add search form template
    First test version of the seach form. Adds class to submit button to
    match bootstrap style.

---
+++ searchform.php
@@ -0,0 +1,10 @@
+<form class="form-search">
+	<input type="text" name="s" id="s" class="input-medium search-query">
+	<button type="submit" class="btn"><?php _e('Search', 'cosmos'); ?></button>
+</form>
+<!--
+<form role="search" method="get" id="searchform" class="form-search" action="<?php echo home_url( '/' ); ?>">
+	<label class="visuallyhidden" for="s"><?php _e('Search for:', 'cosmos'); ?></label>
+	<input type="text" value="" name="s" id="s" class="search-query" placeholder="" />
+	<input type="submit" id="searchsubmit" value="<?php _e('Search', 'cosmos'); ?>" class="btn" />
+</form>-->
