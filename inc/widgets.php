69ebbdb4c403a33e864541861bcd5af76ab38498
Spyros Bazaios <sbazaios@gmail.com>
2012-06-22 15:18:00 +0300

    Add clean markup widget
    It's a simple widget that outputs text or html markup exactly as typed.
    Original code here: http://perishablepress.com/clean-markup-widget

--- inc/widgets.php
+++ inc/widgets.php
@@ -28,6 +28,53 @@ register_sidebar(array(
 	'after_title'   => '</h5>'
 ));
 
+// Clean Markup Widget
+// http://perishablepress.com/clean-markup-widget
+class Clean_Markup extends WP_Widget {
+
+	function __construct() {
+
+		parent::__construct('clean_markup', 'Clean Markup', array('description'=>__('Simple widget for well-formatted markup &amp; text', 'cosmos')));
+	}
+
+	function widget($args, $instance) {
+
+		extract($args, EXTR_SKIP);
+
+		$markup = $instance['markup'];
+
+		if ( $markup ) {
+
+			echo $before_widget;
+			echo $markup;
+			echo $after_widget;
+		}
+	}
+
+	function update($new_instance, $old_instance) {
+
+		$instance = $old_instance;
+		$instance['markup'] = $new_instance['markup'];
+
+		return $instance;
+	}
+
+	function form($instance) {
+
+		if ( $instance ) $markup = esc_attr($instance['markup']);
+		else $markup = __('&lt;p&gt;Clean, well-formatted markup.&lt;/p&gt;', 'cosmos');
+
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id('markup'); ?>"><?php _e('Markup/text'); ?></label>
+			<textarea class="widefat" id="<?php echo $this->get_field_id('markup'); ?>" name="<?php echo $this->get_field_name('markup'); ?>" type="text" rows="16" cols="20" value="<?php echo $markup; ?>"><?php echo $markup; ?></textarea>
+		</p>
+		<?php
+	}
+}
+add_action('widgets_init', create_function('', 'register_widget("Clean_Markup");'));
+
+// Add class to bottom sidebar widgets based on how many they are
 function cosmos_bottom_sidebar_class($params) {
 
 	$sidebar_id = $params[0]['id'];
